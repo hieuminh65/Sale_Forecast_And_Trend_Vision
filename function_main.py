a11c4792f1b58d5f0440fe559c601fbf836fd6c7
Hieu Minh Nguyen <99923860+hieuminh65@users.noreply.github.com>
2023-03-08 22:23:02 -0500

    Update function_main.py

--- function_main.py
+++ function_main.py
@@ -14,16 +14,12 @@ def flip_year(data):
 def filter_input(data):
     while True:
         input1 = str(input("Enter the NAICS Code, Kind of business, or the index to predict: "))
-        for row in (data.iloc[:,0]):
-            if row == input1:
-                i=data.loc[data.iloc[:,0] == input1].index[0]
-                kind = str(data.iloc[i,1])
-                return row, kind
-        for row in (data.iloc[:,1]):
-            if (row.lower() == input1.lower()):
-                i=data.loc[data.iloc[:,1] == row].index[0]
-                code = str(data.iloc[i,0])
-                return code, row
+        if input1 in data['NAICS Code'].tolist():
+            kind = data.loc[data['NAICS Code'] == input1, 'Kind of business'].iloc[0]
+            return input1, kind
+        elif input1 in data['Kind of business'].tolist():
+            code = data.loc[data['Kind of business'] == input1, 'NAICS Code'].iloc[0]
+            return code, input1
         if  int(input1) <= len(data):
             code = str(data.iloc[int(input1),0])
             kind = str(data.iloc[int(input1),1])
@@ -32,8 +28,7 @@ def filter_input(data):
             print("This code or name is invalid.")
         
 
-def draw_scatter_graph(graph, code_kind):
-    code,kind=code_kind
+def draw_scatter_graph(graph, code, kind):
     plt.scatter(x=graph['Year'], y=graph['Revenue'])
     plt.xlabel("Year")
     plt.ylabel("Revenue")
